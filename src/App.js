e27024a5ef46945bd32f6970026020c4201f2f5e
andrew-s-cole <andrew.s.cole@gmail.com>
2022-05-10 15:09:53 -0400

    Updated App.js File

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
-import React, { useState } from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 
 
 function App() {
 
   const [songs, setSongs] = useState([])
+
+  useEffect(() => {
+    getAllSongs();
+  }, [])
+
+  async function getAllSongs(){
+    let response = await axios.get('http://127.0.0.1:8000/api/songs/');
+    setSongs(response.data);
+  }
+
+  async function createSong(newSong){
+
+    let response = await axios.post('http://127.0.0.1:8000/api/songs/', newSong);
+    if(response.status === 201){
+      await getAllSongs();
+    }
+
+  }
+
+  
   
   return (
     <div>
